5beef0eb07282824bca4e8cfcb6ebd615dc85570
Ryan Conrad <camalot@gmail.com>
2025-10-06 20:27:16 -0500

    added some modals and things to UI

--- src/libs/hbs/helpers/strings.ts
+++ src/libs/hbs/helpers/strings.ts
@@ -36,5 +36,26 @@ export default {
     const [value] = Reflection.getArguments(args, ['value'], ['']);
     if (typeof value !== 'string') return '';
     return value.toUpperCase();
+  },
+  jsonPretty: function (this: any, ...args: any[]): string {
+    const [value] = Reflection.getArguments(args, ['value']);
+    if (value === undefined) return 'undefined';
+    if (value === null) return 'null';
+    console.log("jsonPretty value:", value);
+    return JSON.stringify(value, null, 2).trim();
+  },
+  pythonObject: function (this: any, ...args: any[]): any {
+    const [value] = Reflection.getArguments(args, ['value'], ['']);
+    if (typeof value !== 'string') return JSON.stringify(value, null, 2).trim();
+    const fixed = value.replace(/'/g, '"').replace(/(None)([,\s])/g, 'null$2').replace(/(True)([,\s])/g, 'true$2').replace(/(False)([,\s])/g, 'false$2');
+    try {
+      const r = JSON.parse(fixed);
+      console.log("pythonObject parsed value:", r);
+      return JSON.stringify(r, null, 2).trim();
+    } catch (e) {
+      console.error("pythonObject parse error:", e);
+      console.log("pythonObject returning original value:", fixed);
+      return value;
+    }
   }
 }
